f77c257f4806604668d7b5736f2cf882fe4a523c
Juan Prasetya <juanprasetyaaditama@smkwikrama.sch.id>
2024-01-31 13:54:31 +0700

    admin dash & login

---
+++ resources/views/halaman-login.blade.php
@@ -0,0 +1,6 @@
+@extends('back.layout.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Rekap Akademik Wikrama')
+@section('content')
+    
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/halaman-login','halaman-login');
+Route::view('/halaman-dashboard','halaman-dashboard');
+
